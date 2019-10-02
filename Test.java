59560b2d4be3a531f8b4c68f0eea5699a2adddc4
samy <39716963+samykhelifa@users.noreply.github.com>
2019-10-02 15:44:33 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        File f = new File
+                ("C:\\pluralsight\\dir");
+       // f.delete();
+        f.mkdirs();
+     //   System.out.println(f.canExecute());
+     //   System.out.println(File.pathSeparator);
+    }
+}
 
